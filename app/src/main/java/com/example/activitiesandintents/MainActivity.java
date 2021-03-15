14086985b6ba170cfe6b7fd3f83dbdf09d84152f
devansh agarwal <manishyoyo9359@gmail.com>
2021-03-15 11:25:49 +0545

    Task Done

--- app/src/main/java/com/example/activitiesandintents/MainActivity.java
+++ app/src/main/java/com/example/activitiesandintents/MainActivity.java
@@ -4,53 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE            = "com.example.android.activity_second.extra.MESSAGE";
-    public static final int TEXT_REQUEST = 1;
-
-    private EditText mMessageEditText;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    private int mCount = 0;
+    private TextView mShowCount;
+    public static final String EXTRA_COUNT = "helloToastHW1.COUNT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
+        mShowCount = findViewById(R.id.show_count);
     }
 
-
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
+    public void countUp(View view) {
+        mCount ++;
+        if (mShowCount != null){
+            mShowCount.setText(String.format(Locale.ENGLISH, "%d", mCount));
         }
+    }
+
+    public void sayHello(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_COUNT, mCount);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/activitiesandintents/SecondActivity.java
+++ app/src/main/java/com/example/activitiesandintents/SecondActivity.java
@@ -4,34 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
-    public static final String EXTRA_REPLY =
-            "com.example.android.activity_second.extra.REPLY";
-    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        mReply = findViewById(R.id.editText_second);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
+        int mCount = intent.getIntExtra(MainActivity.EXTRA_COUNT, 0);
+        TextView mCountTextView = findViewById(R.id.count_text);
     }
 
-
-    public void returnReply(View view) {
-        String reply = mReply.getText().toString();
-
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
-        setResult(RESULT_OK, replyIntent);
-        finish();
-    }
 }
